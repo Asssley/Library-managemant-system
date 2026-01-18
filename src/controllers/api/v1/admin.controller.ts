64a76b1960606f5789cc98c30d7e87795a7cfe62
Nazar Turbin <turbinnazar@gmail.com>
2026-01-18 03:37:44 +0100

    added asynchronous work to controllers

--- src/controllers/api/v1/admin.controller.ts
+++ src/controllers/api/v1/admin.controller.ts
@@ -3,45 +3,45 @@ import path from "path";
 import { bookRepository } from "../../../buildApp.js";
 import { validateSearchParams, validateNewBook } from "../../../helpers/validators.js";
 
-export const renderMainAdminPage = function (req: Request, res: Response) {
+export const renderMainAdminPage = async function (req: Request, res: Response) {
   let offset = Number(req.query.offset);
   offset = isNaN(offset) ? 0 : offset;
 
-  const books = bookRepository.getBooks(offset)
+  const books = await bookRepository.getBooks(offset)
   res.render(path.join(__dirname, "../views/admin-main.ejs"), books);
 }
 
-export const renderMainPageWithSearch = function (req: Request, res: Response) {
+export const renderMainPageWithSearch = async function (req: Request, res: Response) {
   let offset = Number(req.query.offset);
   offset = isNaN(offset) ? 0 : offset;
 
   const searchParams = validateSearchParams(req);
 
-  const books = bookRepository.searchBooks(offset, searchParams)
+  const books = await bookRepository.searchBooks(offset, searchParams)
   res.render(path.join(__dirname, "../views/admin-main.ejs"), books);
 }
 
-export const renderAddPage = function (req: Request, res: Response) {
+export const renderAddPage = async function (req: Request, res: Response) {
   res.render(path.join(__dirname, "../views/add-page.ejs"));
 }
 
-export const addBook = function (req: Request, res: Response) {
+export const addBook = async function (req: Request, res: Response) {
   const book = validateNewBook(req);
   if (book) {
-    bookRepository.addBook(book);
+    await bookRepository.addBook(book);
     res.status(201).json({ ok: true });
   }
 
   res.status(400).json({ error: "Bad request" })
 }
 
-export const removeBook = function (req: Request, res: Response) {
+export const removeBook = async function (req: Request, res: Response) {
   const bookId = Number(req.body.id);
   if (isNaN(bookId)) {
     res.status(400).json({ error: "Bad request", message: "Id must be a number" })
   }
 
-  const result = bookRepository.removeBook(bookId);
+  const result = await bookRepository.removeBook(bookId);
 
   res.status(200).json({ ok: result });
 }
--- src/controllers/api/v1/user.controller.ts
+++ src/controllers/api/v1/user.controller.ts
@@ -3,43 +3,43 @@ import path from "path";
 import { bookRepository } from "../../../buildApp.js";
 import { validateSearchParams } from "../../../helpers/validators.js";
 
-export const renderMainPage = function (req: Request, res: Response) {
+export const renderMainPage = async function (req: Request, res: Response) {
   let offset = Number(req.query.offset);
   offset = isNaN(offset) ? 0 : offset;
 
-  const books = bookRepository.getBooks(offset)
+  const books = await bookRepository.getBooks(offset)
   res.render(path.join(__dirname, "../views/main.ejs"), books);
 }
 
-export const renderBookPage = function (req: Request, res: Response) {
+export const renderBookPage = async function (req: Request, res: Response) {
   const bookId = Number(req.params.id);
 
   if (isNaN(bookId)) {
     res.render(path.join(__dirname, "../views/404.ejs"));
   }
 
-  const book = bookRepository.getBookById(bookId);
+  const book = await bookRepository.getBookById(bookId);
   res.render(path.join(__dirname, "../views/book.ejs"), book);
 }
 
-export const renderMainPageWithSearch = function (req: Request, res: Response) {
+export const renderMainPageWithSearch = async function (req: Request, res: Response) {
   let offset = Number(req.query.offset);
   offset = isNaN(offset) ? 0 : offset;
 
   const searchParams = validateSearchParams(req);
 
-  const books = bookRepository.searchBooks(offset, searchParams)
+  const books = await bookRepository.searchBooks(offset, searchParams)
   res.render(path.join(__dirname, "../views/main.ejs"), books);
 }
 
-export const increaseTapsCount = function (req: Request, res: Response) {
+export const increaseTapsCount = async function (req: Request, res: Response) {
   const bookId = Number(req.params.id);
 
   if (isNaN(bookId)) {
     res.status(404).json({ error: "Not found" });
   }
 
-  const result = bookRepository.increaseTapsCount(bookId);
+  const result = await bookRepository.increaseTapsCount(bookId);
   if (result) {
     res.status(200).json({ ok: true });
   }
