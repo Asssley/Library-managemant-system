3bd9813e46b03d152c9c253673e580442ac51d2c
Nazar Turbin <turbinnazar@gmail.com>
2026-01-27 04:26:23 +0100

    bug fix

--- src/initParts.ts
+++ src/initParts.ts
@@ -2,9 +2,19 @@ import { pool } from "./db/db.js";
 import AdminRepository from "./repositories/admin.repository.js";
 import BookRepository from "./repositories/book.repository.js";
 import multer from "multer";
-import { getPath } from "./helpers/filesHelpers.js";
+import path from "path";
 
-export const uploadImage = multer({dest: getPath("../view/static/bookImages")});
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "view/static/bookImages");
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname); 
+    const base = path.basename(file.originalname, ext);
+    cb(null, `${base}-${Date.now()}${ext}`);
+  }
+});
 
+export const uploadImage = multer({ storage });
 export const bookRepository = new BookRepository(pool);
 export const adminRepository = new AdminRepository(pool);
