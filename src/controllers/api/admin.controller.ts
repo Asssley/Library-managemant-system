f2443c19f37ff409ad44be7ed1932670e57ec6ea
Nazar Turbin <turbinnazar@gmail.com>
2026-02-08 18:37:51 +0100

    bug fix

--- src/controllers/api/admin.controller.ts
+++ src/controllers/api/admin.controller.ts
@@ -2,7 +2,7 @@ import { type Request, type Response } from "express";
 import { bookRepository } from "../../initParts.js";
 import { validateSearchParams, validateNewBook } from "../../helpers/validators.js";
 import { getPath } from "../../helpers/filesHelpers.js";
-import { convertBookToAdminDTO } from "../../helpers/convertors.js";
+import { convertBookToAdminDTO, toSearchParams } from "../../helpers/convertors.js";
 import fs from "fs";
 
 export const renderMainAdminPage = async function (req: Request, res: Response) {
@@ -12,7 +12,9 @@ export const renderMainAdminPage = async function (req: Request, res: Response) 
   const books = await bookRepository.getBooks(offset);
   const booksDTO = books.map(book => convertBookToAdminDTO(book));
 
-  return res.render(getPath("../view/pages/admin-main.ejs"), booksDTO);
+  const allBooksCount = await bookRepository.getBooksCount();
+
+  return res.render(getPath("../view/pages/admin-main.ejs"), { books: booksDTO, queryString: "", allBooksCount, offset });
 }
 
 export const renderMainPageWithSearch = async function (req: Request, res: Response) {
@@ -24,7 +26,10 @@ export const renderMainPageWithSearch = async function (req: Request, res: Respo
   const books = await bookRepository.searchBooks(offset, searchParams);
   const booksDTO = books.map(book => convertBookToAdminDTO(book));
 
-  return res.render(getPath("../view/pages/admin-main.ejs"), booksDTO);
+  const allBooksCount = await bookRepository.getBooksCount();
+
+  const queryString = new URLSearchParams(toSearchParams(searchParams)).toString();
+  return res.render(getPath("../view/pages/admin-main.ejs"), { books: booksDTO, allBooksCount, queryString, offset });
 }
 
 export const renderAddPage = async function (req: Request, res: Response) {
@@ -41,13 +46,34 @@ export const addBook = async function (req: Request, res: Response) {
 
     return res.status(400).json({ error: "Bad request" })
   } catch (error) {
+
+    if (error instanceof Error && "code" in error) {
+      switch (error.code) {
+
+        case "ER_DUP_ENTRY":
+          return res.status(409).json({
+            error: "Record already exists"
+          });
+
+        case "ER_DATA_TOO_LONG":
+          return res.status(400).json({
+            error: "Value too long"
+          });
+
+        case "ER_WARN_DATA_OUT_OF_RANGE":
+          return res.status(400).json({
+            error: "Number out of range"
+          });
+      }
+    }
+
     throw new Error("Add book error", { cause: error });
   }
 }
 
 export const removeBook = async function (req: Request, res: Response) {
   try {
-    const bookId = Number(req.body.id);
+    const bookId = Number(req.params.id);
     if (isNaN(bookId)) {
       return res.status(400).json({ error: "Bad request", message: "Id must be a number" })
     }
@@ -55,7 +81,9 @@ export const removeBook = async function (req: Request, res: Response) {
     const book = await bookRepository.getBookById(bookId);
     if (book?.imagePath) {
       fs.unlink(getPath(["..", book.imagePath]), err => {
-        throw new Error("Remove book image error", { cause: err });
+        if (err) {
+          throw new Error("Remove book image error", { cause: err });
+        }
       });
     }
 
